5a885d9b53c3c70eea1852db04e87772dd6d2204
pankaj426 <58098844+pankaj426@users.noreply.github.com>
2022-08-29 16:01:57 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -2,8 +2,8 @@
 resource "aws_db_subnet_group" "mariadb-subnets" {
   name        = "mariadb-subnets"
   description = "Amazon RDS subnet group"
-  subnet_ids  = [aws_subnet.levelupvpc-private-1.id, aws_subnet.levelupvpc-private-2.id]
-}
+  subnet_ids  = "aws_subnet.demo[*].id"
+
 
 #RDS Parameters
 resource "aws_db_parameter_group" "levelup-mariadb-parameters" {
@@ -33,7 +33,7 @@ resource "aws_db_instance" "levelup-mariadb" {
   vpc_security_group_ids  = [aws_security_group.allow-mariadb.id]
   storage_type            = "gp2"
   backup_retention_period = 30                                          # how long you’re going to keep your backups
-  availability_zone       = aws_subnet.levelupvpc-private-1.availability_zone # prefered AZ
+  availability_zone       = us-east-1 # prefered AZ
   skip_final_snapshot     = true                                        # skip final snapshot when doing terraform destroy
   
   tags = {
